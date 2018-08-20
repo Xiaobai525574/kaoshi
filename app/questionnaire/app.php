ac6a9740784fbc7fbdcd343e0bef6ddebc97bd20
xiao <893530635@qq.com>
2018-08-20 18:04:31 +0800

    questionnaire\app bug

--- app/questionnaire/app.php
+++ app/questionnaire/app.php
@@ -18,34 +18,34 @@ class app
 
         $this->session = $this->G->make('session');
         $this->questionnaire = $this->G->make('questionnaire');
-        $this->user = $this->G->make('user','user');
-        $this->comment = $this->G->make('comment','questionnaire');
+        $this->user = $this->G->make('user', 'user');
+        $this->comment = $this->G->make('comment', 'questionnaire');
         $this->_user = $_user = $this->session->getSessionUser();
         $group = $this->user->getGroupById($_user['sessiongroupid']);
         /*登录验证*/
-        if($group['groupid'] != 1)
-        {
-            if($this->ev->get('userhash'))
-            exit(json_encode(array(
+        if ($group['groupid'] != 1) {
+            if ($this->ev->get('userhash')) {
+                exit(json_encode(array(
                     'statusCode' => 300,
                     "message" => "请您重新登录",
                     "callbackType" => 'forward',
                     "forwardUrl" => "index.php?core-master-login"
                 )));
-        else
-            header("location:index.php?core-master-login");
-            exit();
+            } else {
+                header("location:index.php?core-master-login");
+                exit;
+            }
         }
 
         $this->question = $this->G->make('question', 'questionnaire');
         $this->comment = $this->G->make('comment', 'questionnaire');
 
 
-        $this->tpl->assign('_user',$this->user->getUserById($this->_user['sessionuserid']));
+        $this->tpl->assign('_user', $this->user->getUserById($this->_user['sessionuserid']));
 
         $this->category = $this->G->make('category');
-        $rcats = $this->category->getCategoriesByArgs(array(array("AND","catparent = '0'")));
-        $this->tpl->assign('rcats',$rcats);
+        $rcats = $this->category->getCategoriesByArgs(array(array("AND", "catparent = '0'")));
+        $this->tpl->assign('rcats', $rcats);
     }
 }
 
